abcad2818636f75f4f155e1a55c9f41f16daa043
Sam Corcos <sam@andaril.com>
2014-11-01 14:32:55 -0400

    initial commit, figured out some of the parameters

---
+++ monty.js
@@ -0,0 +1,16 @@
+var games = 1000;
+
+    var winningDoor = function () {
+        return
+    }
+
+
+for (i=0;i<games;i++) {     //this will run for 1000 games
+
+}
+
+
+// the output should be:
+// 1) # of games played (1000)
+// 2) # of games won when you dont switch doors
+// 3) # of games won when you do switch doors
