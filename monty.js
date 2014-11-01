cf3ae8ac0478e8f7febf98e20435115b1294a7cd
Sam Corcos <sam@andaril.com>
2014-11-01 15:09:43 -0400

    solved monty hall

--- monty.js
+++ monty.js
@@ -1,16 +1,24 @@
-var games = 1000;
+var games = 1000, noChange = 0, change = 0;
 
-    var winningDoor = function () {
-        return
+var randomDoor = function() {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+for (i = 0; i < games; i++) {
+    var winningDoor = randomDoor();
+    var chosenDoor = randomDoor();
+    if (winningDoor == chosenDoor) {    // If you pick the right door and dont change, you win.
+        noChange += 1;
     }
+    if (winningDoor != chosenDoor) {    // This is where it gets interesting! If you originally picked the wrong door, another wrong door would be opened, leaving your only remaining option: the correct door!
+        change += 1;
+    }
+}
 
-
-for (i=0;i<games;i++) {     //this will run for 1000 games
+for (i = 0; i < games; i++) {
 
 }
 
 
-// the output should be:
-// 1) # of games played (1000)
-// 2) # of games won when you dont switch doors
-// 3) # of games won when you do switch doors
+console.log(noChange + " wins out of 1000 games if you do not change doors.");
+console.log(change + " wins out of 1000 games if you do change doors.");
